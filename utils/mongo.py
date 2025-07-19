c9d626a0631c6fac8b15c64753c84017eee5f942
Andrew R <25roband@cbc.sa.edu.au>
2025-07-19 21:51:36 +0930

    remove sensitive secrets

--- utils/mongo.py
+++
@@ -1,8 +0,0 @@
-import pymongo
-
-client = pymongo.MongoClient("mongodb+srv://andrewedwardrobinson:MongoDB@cluster0.4zdcdya.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-
-db = client['breeze_bot']
-
-
-general = db["pterodactyl"]
